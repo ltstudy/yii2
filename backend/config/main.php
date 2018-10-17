8c386f000a2e00654b0f88e2424c2401dfb2b771
ltstudy@sina.com <ltstudy@sina.com>
2018-10-18 01:24:19 +0800

    美化路由

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,6 +37,19 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,  //美化url==ture
+            'enableStrictParsing' => false,  //不启用严格解析
+            'showScriptName' => false,   //隐藏index.php
+            'rules' => [
+                '<module:\w+>/<controller:\w+>/<id:\d+>' => '<module>/<controller>/view',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '/m-api/' => '/meanpan-api/index', // 默认index
+                '/mp-api/<action>' => '/meanpan-api/<action>',   // /mp-api/*==>/meanpan-api/*
+            ],
+        ]
+
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
