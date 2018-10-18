ab47651ef7cadd636efd9c69bafd614b72e31bfa
ltstudy@sina.com <ltstudy@sina.com>
2018-10-18 10:31:38 +0800

    修改main

--- Websock.php
+++ Websock.php
@@ -13,11 +13,13 @@ $config = [
     'setting' => [
         'daemonize'=>0,
         'worker_num'=>1,
+        'pid_file' => __DIR__ . '/backend/runtime/server.pid',
+
     ],
-//    'pid_file' => __DIR__ . '/backend/runtime/server.pid',
 //    'pid_file' => __DIR__ . '/console/runtime/testHttp.pid',
 //    'log_file' => __DIR__ . '/console/runtime/logs/swoole.log',
 ];
+
 
 
 Server::run($config,function (Server $server){
